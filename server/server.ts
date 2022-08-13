f9ce7b6ee95ad0d4fe8f0b8e71d5b0de32062734
David Oluyale <oluyaled@gmail.com>
2022-08-13 23:14:28 +0100

    backend service completed -- added readme

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import app  from "./src/app";
 
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Applications is running on port ${port}.`);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,8 @@
-import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
 const routes = require('./routes');
-require('dotenv').config();
-const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
